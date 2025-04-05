3037d4e52fc5e15cdbd9f949738a2eec848ae138
Thang Le <lequocthang201197@gmail.com>
2025-04-05 10:02:24 +0700

    update to deploy

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,4 @@
-export const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:8000/api";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const API_ENDPOINTS = {
   LOGIN: "/auth/login",
